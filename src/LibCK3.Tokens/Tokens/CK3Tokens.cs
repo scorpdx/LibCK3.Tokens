55890b166768d5ce9cf96cacb8229200a85a045d
J. Zebedee <zebedee@code.gripe>
2021-11-19 10:35:51 -0500

    Break out token parsing into Meltwater

--- src/LibCK3.Tokens/Tokens/CK3Tokens.cs
+++ src/LibCK3.Tokens/Tokens/CK3Tokens.cs
@@ -9,26 +9,16 @@ namespace LibCK3.Tokens
 {
     public static partial class CK3Tokens
     {
-        private static readonly string TokenFilePath = Path.Join("Assets", "1.4.4.tok.msgpack");
-
-        private static readonly EmbeddedFileProvider _provider = new(typeof(CK3Tokens).Assembly);
-        private static Dictionary<string, ushort> ReadEmbeddedTokens()
-        {
-            var tokenFile = _provider.GetFileInfo(TokenFilePath);
-            using var fs = tokenFile.CreateReadStream();
-
-            var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
-            return MessagePackSerializer.Deserialize<Dictionary<string, ushort>>(fs, lz4Options);
-        }
-
         private static readonly Lazy<Dictionary<ushort, JsonEncodedText>> _tokens
             = new(() => _tokenNames.Value.ToDictionary(kvp => kvp.Value, kvp => JsonEncodedText.Encode(kvp.Key)));
+
         /// <summary>
         /// 1.4.4 2021-06-29 79ad Hotfix 
         /// </summary>
         public static IReadOnlyDictionary<ushort, JsonEncodedText> Tokens => _tokens.Value;
 
         private static readonly Lazy<Dictionary<string, ushort>> _tokenNames = new(ReadEmbeddedTokens);
+
         /// <summary>
         /// 1.4.4 2021-06-29 79ad Hotfix 
         /// </summary>
---
+++ src/Meltwater/RawTokenProvider.cs
@@ -0,0 +1,20 @@
+﻿using MessagePack;
+using Microsoft.Extensions.FileProviders;
+
+namespace Meltwater
+{
+    internal static class RawTokenProvider
+    {
+        private static readonly string TokenFilePath = Path.Join("Assets", "1.4.4.tok.msgpack");
+
+        internal static Dictionary<string, ushort> ReadEmbeddedTokens()
+        {
+            var provider = new EmbeddedFileProvider(typeof(RawTokenProvider).Assembly);
+            var tokenFile = provider.GetFileInfo(TokenFilePath);
+            using var fs = tokenFile.CreateReadStream();
+
+            var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
+            return MessagePackSerializer.Deserialize<Dictionary<string, ushort>>(fs, lz4Options);
+        }
+    }
+}
