9c87b5a50e4316d578310d4e87a476f9c7ddd6eb
J. Zebedee <zebedee@code.gripe>
2021-11-19 16:52:06 -0500

    Update to rely on Meltwater generator

--- src/Meltwater/CK3TokensGenerator.cs
+++ src/Meltwater/CK3TokensGenerator.cs
@@ -1,135 +1,107 @@
 ﻿using Microsoft.CodeAnalysis;
-using System.Collections.ObjectModel;
 
-namespace Meltwater
+namespace Meltwater;
+
+[Generator]
+public class CK3TokensGenerator : ISourceGenerator
 {
-    [Generator]
-    public class CK3TokensGenerator : ISourceGenerator
+    const string @namespace = "LibCK3.Tokens";
+    const string @class = "CK3Tokens";
+
+    private Dictionary<ushort, string> _rawTokens;
+
+    private static IEnumerable<string> ToRakalyFormat(IReadOnlyDictionary<ushort, string> tokenDict)
+        => tokenDict.Select(kvp => string.Format("0x{0:X4} {1}", kvp.Key, kvp.Value));
+
+    private static IEnumerable<string> ToLibCK3Format(IReadOnlyDictionary<ushort, string> tokenDict)
     {
-        private Dictionary<string, ushort> _rawTokens;
-        private static Dictionary<ushort, string> ParseTokens(IEnumerable<string> lines)
-            => lines.Select(line => line.Split(' '))
-            .Where(a => a.Length == 2)
-            .Select(a => new
-            {
-                ID = ushort.Parse(a[1]),
-                Name = a[0]
-            })
-            //.Where(a => a.ID >= Offset)
-            .ToDictionary(a => /*checked((ushort)(a.ID - Offset))*/a.ID, a => a.Name);
-
-        private static IEnumerable<string> ToRakalyFormat(IReadOnlyDictionary<ushort, string> tokenDict)
-            => tokenDict.Select(kvp => string.Format("0x{0:X4} {1}", kvp.Key, kvp.Value));
-
-        private static IEnumerable<string> ToLibCK3Format(string version, IReadOnlyDictionary<ushort, string> tokenDict)
+        yield return "using System.Collections.Generic;";
+        yield return "using System.Text.Json;";
+        yield return $"namespace {@namespace};";
+        yield return $"public static partial class {@class}";
+        yield return "{";
+        yield return $"    private static ReadOnlyDictionary<ushort, JsonEncodedText> _tokens = new(new Dictionary<ushort, JsonEncodedText>";
+        yield return "    {";
+        foreach (var kvp in tokenDict)
         {
-            yield return "using System.Collections.Generic;";
-            yield return "using System.Text.Json;";
-            yield return "namespace LibCK3.Tokens";
-            yield return "{";
-            yield return "    public static partial class CK3Tokens";
-            yield return "    {";
-            yield return $"        private static IReadOnlyDictionary<ushort, JsonEncodedText> Tokens{version.Replace('.', '_')} {{ get; }} = new Dictionary<ushort, JsonEncodedText>";
-            yield return "        {";
-            foreach (var kvp in tokenDict)
-            {
-                yield return $"            {{0x{kvp.Key:X4}, JsonEncodedText.Encode(\"{kvp.Value}\")}},";
-            }
-            yield return "        };";
-            yield return "    }";
-            yield return "}";
+            yield return $"        {{0x{kvp.Key:X4}, JsonEncodedText.Encode(\"{kvp.Value}\")}},";
         }
+        yield return "    });";
+        yield return "}";
+    }
 
-        private static IEnumerable<string> ToLibCK3NameFormat(string version, IReadOnlyDictionary<ushort, string> tokenDict)
+    private static IEnumerable<string> ToLibCK3NameFormat(IReadOnlyDictionary<ushort, string> tokenDict)
+    {
+        yield return "using System.Collections.Generic;";
+        yield return "using System.Text.Json;";
+        yield return "namespace LibCK3.Tokens";
+        yield return "{";
+        yield return "    public static partial class CK3Tokens";
+        yield return "    {";
+        yield return $"        private static ReadOnlyDictionary<string, ushort> _tokenNames = new(new Dictionary<string, ushort>";
+        yield return "        {";
+        foreach (var kvp in tokenDict)
         {
-            yield return "using System.Collections.Generic;";
-            yield return "using System.Text.Json;";
-            yield return "namespace LibCK3.Tokens";
-            yield return "{";
-            yield return "    public static partial class CK3Tokens";
-            yield return "    {";
-            yield return $"        private static IReadOnlyDictionary<ushort, JsonEncodedText> Tokens{version.Replace('.', '_')} {{ get; }} = new Dictionary<string, ushort>";
-            yield return "        {";
-            foreach (var kvp in tokenDict)
-            {
-                yield return $"            {{\"{kvp.Value}\", 0x{kvp.Key:X4}}},";
-            }
-            yield return "        };";
-            yield return "    }";
-            yield return "}";
+            yield return $"            {{\"{kvp.Value}\", 0x{kvp.Key:X4}}},";
         }
+        yield return "        });";
+        yield return "    }";
+        yield return "}";
+    }
 
-        private static string san(string text) => char.IsDigit(text = text.Replace("_", "__").Replace(':', '_').Replace('.', '_')
-                                                                          .Replace('&', '_').Replace('}', '_')
-                                                                          .Replace('-', '_'), 0) ? $"v{text}" : text;
+    //private static string san(string text) => char.IsDigit(text = text.Replace("_", "__").Replace(':', '_').Replace('.', '_')
+    //                                                                  .Replace('&', '_').Replace('}', '_')
+    //                                                                  .Replace('-', '_'), 0) ? $"v{text}" : text;
 
-        private static IEnumerable<string> ToLibCK3TokenDefinitions(IEnumerable<(string version, ushort id, string token)> allTokens)
-        {
-            yield return "using System.Collections.Generic;";
-            yield return "using System.Text.Json;";
-            yield return "namespace LibCK3.Tokens";
-            yield return "{";
-            yield return "    public static partial class CK3Tokens";
-            yield return "    {";
-            foreach (var token in allTokens.Select(a => a.token).Where(a => !string.IsNullOrWhiteSpace(a)).Distinct())
-            {
-                yield return $"        private static JsonEncodedText? @{san(token)};";
-            }
-            yield return "    }";
-            yield return "}";
-        }
+    //private static IEnumerable<string> ToLibCK3TokenDefinitions(IEnumerable<(string version, ushort id, string token)> allTokens)
+    //{
+    //    yield return "using System.Collections.Generic;";
+    //    yield return "using System.Text.Json;";
+    //    yield return "namespace LibCK3.Tokens";
+    //    yield return "{";
+    //    yield return "    public static partial class CK3Tokens";
+    //    yield return "    {";
+    //    foreach (var token in allTokens.Select(a => a.token).Where(a => !string.IsNullOrWhiteSpace(a)).Distinct())
+    //    {
+    //        yield return $"        private static JsonEncodedText? @{san(token)};";
+    //    }
+    //    yield return "    }";
+    //    yield return "}";
+    //}
 
-        private static IEnumerable<string> ToLibCK3TokenTable(string version, IReadOnlyDictionary<ushort, string> tokens)
-        {
-            const string throwFragment = @"throw new InvalidOperationException(""Token identifier is null"")";
+    //private static IEnumerable<string> ToLibCK3TokenTable(IReadOnlyDictionary<ushort, string> tokens)
+    //{
+    //    const string throwFragment = @"throw new InvalidOperationException(""Token identifier is null"")";
+    //    const string throwName = "ThrowTN";
 
-            yield return "using System;";
-            yield return "using System.Collections.Generic;";
-            yield return "using System.Text.Json;";
-            yield return "namespace LibCK3.Tokens";
-            yield return "{";
-            yield return "    public static partial class CK3Tokens";
-            yield return "    {";
-            yield return $"        private static JsonEncodedText ParseToken_{san(version)}(ushort ck3id)";
-            yield return "          => ck3id switch";
-            yield return "          {";
-            foreach (var a in tokens)
-            {
-                yield return string.IsNullOrWhiteSpace(a.Value)
-                    ? $"              {a.Key} => {throwFragment},"
-                    : $"              {a.Key} => JsonEncodedText.Encode(\"{a.Value}\"),";
-            }
-            yield return @"              _ => throw new InvalidOperationException(""No tokens matched"")";
-            yield return "          };";
-            yield return "    }";
-            yield return "}";
-        }
+    //    yield return "using System;";
+    //    yield return "using System.Collections.Generic;";
+    //    yield return "using System.Text.Json;";
+    //    yield return "namespace LibCK3.Tokens";
+    //    yield return "{";
+    //    yield return "    public static partial class CK3Tokens";
+    //    yield return "    {";
+    //    yield return $"        private static void {throwName}() => {throwFragment};";
+    //    yield return $"        public static JsonEncodedText ParseToken(ushort ck3id)";
+    //    yield return "          => ck3id switch";
+    //    yield return "          {";
+    //    foreach (var a in tokens.Where(x => !string.IsNullOrWhiteSpace(x.Value)))
+    //    {
+    //        yield return $"              {a.Key} => JsonEncodedText.Encode(\"{a.Value}\"),";
+    //    }
+    //    yield return @"              _ => throw new InvalidOperationException(""Not a valid token identifier"")";
+    //    yield return "          };";
+    //    yield return "    }";
+    //    yield return "}";
+    //}
 
-        public void Execute(GeneratorExecutionContext context)
-        {
-            const string @namespace = "LibCK3.Tokens";
-            const string @class = "CK3Tokens";
-
-            // build up the source code
-            string source = $@"
-using System;
-
-namespace {@namespace}
-{{
-    public static partial class {@class}
-    {{
-        public static ReadOnlyDictionary<ushort, JsonEncodedText> Tokens {{ get; }} = new(new Dictionary<ushort, JsonEncodedText> {{
-            {{ }}
-        }}
-    }}
-}}
-";
-            // add the source code to the compilation
-            context.AddSource("CK3Tokens.Strings.cs", source);
-        }
-        public void Initialize(GeneratorInitializationContext context)
-        {
-            _rawTokens = RawTokenProvider.ReadEmbeddedTokens();
-        }
+    public void Execute(GeneratorExecutionContext context)
+    {
+        context.AddSource("CK3Tokens.Json.cs", string.Join(Environment.NewLine, ToLibCK3Format(_rawTokens)));
+        context.AddSource("CK3Tokens.Names.cs", string.Join(Environment.NewLine, ToLibCK3NameFormat(_rawTokens)));
+    }
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        _rawTokens = RawTokenProvider.ReadEmbeddedTokens().ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
     }
 }
