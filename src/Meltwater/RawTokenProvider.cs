c390aa7903ba5200839b9e784c54d8dde0d5f6e4
J. Zebedee <zebedee@code.gripe>
2021-11-19 17:15:31 -0500

    Use regular tokens instead of MessagePack

--- src/Meltwater/RawTokenProvider.cs
+++ src/Meltwater/RawTokenProvider.cs
@@ -1,19 +1,34 @@
-﻿using MessagePack;
-using Microsoft.Extensions.FileProviders;
+﻿using Microsoft.Extensions.FileProviders;
 
 namespace Meltwater;
 
 internal static class RawTokenProvider
 {
-    private const string TokenFilePath = "Assets/1.4.4.tok.msgpack";
+    private const string TokenFilePath = "Assets/1.4.4.tok";
 
-    internal static Dictionary<string, ushort> ReadEmbeddedTokens()
+    private static Dictionary<ushort, string> ParseTokens(IEnumerable<string> lines)
+        => lines.Select(line => line.Split(' '))
+                .Where(a => a.Length == 2)
+                .Select(a => new
+                {
+                    ID = ushort.Parse(a[1]),
+                    Name = a[0]
+                })
+                .ToDictionary(a => a.ID, a => a.Name);
+
+    private static IEnumerable<string> ReadEmbeddedTokenLines()
     {
         var provider = new EmbeddedFileProvider(typeof(RawTokenProvider).Assembly);
         var tokenFile = provider.GetFileInfo(TokenFilePath);
         using var fs = tokenFile.CreateReadStream();
+        using var sr = new StreamReader(fs);
 
-        var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
-        return MessagePackSerializer.Deserialize<Dictionary<string, ushort>>(fs, lz4Options);
+        string line;
+        while((line = sr.ReadLine()) != null)
+        {
+            yield return line;
+        }
     }
+
+    internal static Dictionary<ushort, string> ReadEmbeddedTokens() => ParseTokens(ReadEmbeddedTokenLines());
 }
