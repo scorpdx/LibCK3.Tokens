d67539c575a87a057b0a01f5ce9de77f176413af
J. Zebedee <zebedee@code.gripe>
2021-11-19 10:35:51 -0500

    Break out token parsing into Meltwater

---
+++ src/Meltwater/CK3TokensGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.ObjectModel;
+
+namespace Meltwater
+{
+    [Generator]
+    public class CK3TokensGenerator : ISourceGenerator
+    {
+        private Dictionary<string, ushort> _rawTokens;
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            const string @namespace = "LibCK3.Tokens";
+            const string @class = "CK3Tokens";
+
+            // build up the source code
+            string source = $@"
+using System;
+
+namespace {@namespace}
+{{
+    public static partial class {@class}
+    {{
+        public static ReadOnlyDictionary<ushort, JsonEncodedText> Tokens {{ get; }} = new(new Dictionary<ushort, JsonEncodedText> {{
+            {{ }}
+        }}
+    }}
+}}
+";
+            // add the source code to the compilation
+            context.AddSource("CK3Tokens.Strings.cs", source);
+        }
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            _rawTokens = RawTokenProvider.ReadEmbeddedTokens();
+        }
+    }
+}
