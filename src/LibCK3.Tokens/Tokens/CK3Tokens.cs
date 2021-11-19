d9c8e7a9b526348bf66054e308ba1d33ad7a92cc
J. Zebedee <zebedee@code.gripe>
2021-11-19 16:52:06 -0500

    Update to rely on Meltwater generator

--- src/LibCK3.Tokens/Tokens/CK3Tokens.cs
+++ src/LibCK3.Tokens/Tokens/CK3Tokens.cs
@@ -1,27 +1,17 @@
-﻿using MessagePack;
-using Microsoft.Extensions.FileProviders;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
 using System.Text.Json;
 namespace LibCK3.Tokens
 {
     public static partial class CK3Tokens
     {
-        private static readonly Lazy<Dictionary<ushort, JsonEncodedText>> _tokens
-            = new(() => _tokenNames.Value.ToDictionary(kvp => kvp.Value, kvp => JsonEncodedText.Encode(kvp.Key)));
+        /// <summary>
+        /// 1.4.4 2021-06-29 79ad Hotfix 
+        /// </summary>
+        public static ReadOnlyDictionary<ushort, JsonEncodedText> Tokens => _tokens;
 
         /// <summary>
         /// 1.4.4 2021-06-29 79ad Hotfix 
         /// </summary>
-        public static IReadOnlyDictionary<ushort, JsonEncodedText> Tokens => _tokens.Value;
-
-        private static readonly Lazy<Dictionary<string, ushort>> _tokenNames = new(ReadEmbeddedTokens);
-
-        /// <summary>
-        /// 1.4.4 2021-06-29 79ad Hotfix 
-        /// </summary>
-        public static IReadOnlyDictionary<string, ushort> TokenNames => _tokenNames.Value;
+        public static ReadOnlyDictionary<string, ushort> TokenNames => _tokenNames;
     }
 }
--- src/Meltwater/RawTokenProvider.cs
+++ src/Meltwater/RawTokenProvider.cs
@@ -1,20 +1,19 @@
 ﻿using MessagePack;
 using Microsoft.Extensions.FileProviders;
 
-namespace Meltwater
+namespace Meltwater;
+
+internal static class RawTokenProvider
 {
-    internal static class RawTokenProvider
+    private const string TokenFilePath = "Assets/1.4.4.tok.msgpack";
+
+    internal static Dictionary<string, ushort> ReadEmbeddedTokens()
     {
-        private static readonly string TokenFilePath = Path.Join("Assets", "1.4.4.tok.msgpack");
+        var provider = new EmbeddedFileProvider(typeof(RawTokenProvider).Assembly);
+        var tokenFile = provider.GetFileInfo(TokenFilePath);
+        using var fs = tokenFile.CreateReadStream();
 
-        internal static Dictionary<string, ushort> ReadEmbeddedTokens()
-        {
-            var provider = new EmbeddedFileProvider(typeof(RawTokenProvider).Assembly);
-            var tokenFile = provider.GetFileInfo(TokenFilePath);
-            using var fs = tokenFile.CreateReadStream();
-
-            var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
-            return MessagePackSerializer.Deserialize<Dictionary<string, ushort>>(fs, lz4Options);
-        }
+        var lz4Options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
+        return MessagePackSerializer.Deserialize<Dictionary<string, ushort>>(fs, lz4Options);
     }
 }
